c1e9994af66271c90b571aa11a40e20f7b14d45a
Ana Maciel <casmaciel96@gmail.com>
2022-01-14 19:58:41 -0300

    implementar interatividade para a pag

--- script.js
+++ script.js
@@ -1,6 +1,37 @@
-const img =document.querySelector("img");
-    const botao =document.querySelector("button");
-    const nomeDoPersonagem =document.querySelector("#nome");
-    const especie =document.querySelector("#especie");
-    const status =document.querySelector("#status");
-    
+const img = document.querySelector('img');
+    const botao = document.querySelector('button');
+    const nomeDoPersonagem = document.querySelector('#nome');
+    const especie = document.querySelector('#especie');
+    const condicao = document.querySelector('#status');
+
+    traduzirCondicao = (data) => {
+        if(data.status == 'unknown'){
+            return 'Não sabemos';
+        }else if(data.status == 'Alive'){
+            return 'Sim';
+        }else {
+            return 'Não. Está morto';
+        }
+    }
+    gerarValorAleatorio = () => {
+        return Math.floor(Math.random() * 671);
+    }
+
+    pegarPersonagem = () => {
+        let numeroAleatorio = gerarValorAletorio();
+        return fetch(`https://rickandmortyapi.com/api/character/${numeroAleatorio}`, {
+            method:'GET',
+            headers: {
+                Accept: 'application/json',
+                "Content-type": 'application/json'
+            }
+        }).then((response) => response.json()).then((data) => {
+            imagem.src = data.image;
+            imagem.alt = data.name;
+            nomeDoPersonagem.innerHTML = data.name;
+            especie.innerHTML = data.species;
+            condicao.innerHTML = traduzirCondicao(data);
+        });
+    }
+
+    botao.onclick = pegarPersonagem;
