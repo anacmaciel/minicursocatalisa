fb5d51e9639a309d6dae4297d01bca3e91708c6d
Ana Maciel <casmaciel96@gmail.com>
2022-01-15 15:20:48 -0300

    Desenvolvida solucao do desafio

--- script.js
+++ script.js
@@ -1,15 +1,13 @@
-const imagem = document.querySelector('img');
+const numeroPersonagens = 3;
+const conteudo = document.querySelector('#conteudo');
 const botao = document.querySelector('button');
-const nomeDoPersonagem = document.querySelector('#nome');
-const especie = document.querySelector('#especie');
-const condicao = document.querySelector('#status');
 
 traduzirCondicao = (data) => {
-    if(data.status == 'unknown'){
+    if (data.status == 'unknown') {
         return 'Não sabemos';
-    }else if(data.status == 'Alive'){
+    } else if (data.status == 'Alive') {
         return 'Sim';
-    }else {
+    } else {
         return 'Não. Está morto';
     }
 }
@@ -18,21 +16,42 @@ gerarValorAleatorio = () => {
     return Math.floor(Math.random() * 671);
 }
 
+criarItem = (texto) => {
+    const li = document.createElement("li");
+    const element = document.createElement("p");
+    element.innerHTML = texto;
+    li.appendChild(element);
+    return li;
+}
+
 pegarPersonagem = () => {
-    let numeroAleatorio = gerarValorAleatorio();
-        return fetch(`https://rickandmortyapi.com/api/character/${numeroAleatorio}`, {
-        method:'GET',
-        headers: {
-            Accept: 'application/json',
-            "Content-type": 'application/json'
-        }
-    }).then((response) => response.json()).then((data) => {
-        imagem.src = data.image;
-        imagem.alt = data.name;
-        nomeDoPersonagem.innerHTML = data.name;
-        especie.innerHTML = data.species;
-        condicao.innerHTML = traduzirCondicao(data);
-    });
+    conteudo.innerHTML = "";
+    for (i = 0; i < numeroPersonagens; i++) {
+        let numeroAleatorio = gerarValorAleatorio();
+        fetch(`https://rickandmortyapi.com/api/character/${numeroAleatorio}`, {
+            method: 'GET',
+            headers: {
+                Accept: 'application/json',
+                "Content-type": 'application/json'
+            }
+        }).then((response) => response.json()).then((data) => {
+            const imagem = document.createElement("img");
+            imagem.src = data.image;
+            imagem.alt = data.name;
+            const container = document.createElement("ul");
+            container.classList.add("detalhes-container");
+            const nomeDoPersonagem = criarItem(data.name);
+            const especie = criarItem(data.species);
+            const condicao = criarItem(traduzirCondicao(data));
+            container.appendChild(nomeDoPersonagem);
+            container.appendChild(especie);
+            container.appendChild(condicao);
+            const personagem = document.createElement("artiicle");
+            personagem.appendChild(imagem);
+            personagem.appendChild(container);
+            conteudo.appendChild(personagem);
+        });
+    }
 }
 
 botao.onclick = pegarPersonagem;
